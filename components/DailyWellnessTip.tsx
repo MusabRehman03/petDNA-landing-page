d93db8f720305de0a7378ff09a8b1e6fecac4e73
Musab Ur Rehman <rehmanmusab0302@gmail.com>
2025-07-06 15:01:14 +0500

    fix: fixed hover effect for button

--- components/DailyWellnessTip.tsx
+++ components/DailyWellnessTip.tsx
@@ -44,7 +44,7 @@ const DailyWellnessTip = () => {
               <div>
                 <p className="font-bold">{tip.text}</p>
               </div>
-              <button className="px-4 py-0.5 text-[#332a24] rounded-4xl mt-2 hover:bg-[#9A7E6F] cursor-pointer bg-[#e1d0c6]">
+              <button className="px-4 py-0.5 text-[#332a24] hover:text-white rounded-4xl mt-2 hover:bg-[#9A7E6F] cursor-pointer bg-[#e1d0c6]">
                 Learn More
               </button>
             </div>
